5a00803d39adf6c540b1c56fa86d3f312420fbcc
ulskmo <128420582+ulaskmo@users.noreply.github.com>
2024-09-25 10:25:50 +0100

    finito

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
-import express, {Application, Request, Response} from "express" ;
+import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
+import userRoutes from './routes/users'; // Import the user routes
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
-app.use(morgan("tiny"));
+app.use(morgan("tiny")); // Keep the morgan middleware
 
-app.get("/ping", async (_req : Request, res: Response) => {
-res.send({
-    message: "hello from helo",
+// Tell the app to use the userRoutes
+app.use('/api/v1/users', userRoutes); 
+
+app.get("/ping", async (_req: Request, res: Response) => {
+    res.send({
+        message: "hello from helllllo",
     });
 });
 
- app.listen(PORT, () => {
-    console.log("Server is running on port  --", PORT);
-    });
+app.get('/bananas', async (_req: Request, res: Response) => {
+    res.send('hello world, this is bananas');
+});
 
-    app.get('/bananas', async (_req : Request, res: Response) => 
-        res.send('hello world, this is bananas'));
-    
+app.listen(PORT, () => {
+    console.log("Server is running on port --", PORT);
+});
